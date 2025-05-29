e6593973ad3d328c46455d0220ac27c3745e4144
catseng6606 <125848@mail.tku.edu.tw>
2025-05-29 13:39:03 +0800

    markdown to json

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
     var surveyJSON = {
-        "locale": "zh-tw", // 預設語系為繁體中文
+        "locale": "zh-tw",
         "locales": {
             "en": {
                 "page1": {
@@ -8,7 +8,7 @@ $(function() {
                         "language": {
                             "title": "Please select your preferred language",
                             "choices": [
-                                { "value": "zh-tw", "text": "中文" },
+                                { "value": "zh-tw", "text": "Chinese" },
                                 { "value": "en", "text": "English" }
                             ]
                         }
@@ -16,14 +16,36 @@ $(function() {
                 },
                 "page2": {
                     "elements": {
-                        "name": {
-                            "title": "What is your name?"
-                        },
-                        "age": {
-                            "title": "How old are you?"
+                        "question1": {
+                            "title": "This is Survey Question 1."
                         }
                     }
-                }
+                },
+                "page3": {
+                    "elements": {
+                        "question2": {
+                            "title": "This is Survey Question 2."
+                        }
+                    }
+                },
+                "page4": {
+                    "elements": {
+                        "conditionA": {
+                            "title": "Is condition A met?",
+                            "choices": [
+                                { "value": "yes", "text": "Yes" },
+                                { "value": "no", "text": "No" }
+                            ]
+                        }
+                    }
+                },
+                "result_a": {
+                    "description": "Result Page A: Congratulations! You meet the conditions."
+                },
+                "result_b": {
+                    "description": "Result Page B: Sorry, you do not meet the conditions."
+                },
+                "completeText": "Complete"
             },
             "zh-tw": {
                 "page1": {
@@ -39,14 +61,36 @@ $(function() {
                 },
                 "page2": {
                     "elements": {
-                        "name": {
-                            "title": "您的姓名是？"
-                        },
-                        "age": {
-                            "title": "您的年齡是？"
+                        "question1": {
+                            "title": "這是問卷問題 1。"
                         }
                     }
-                }
+                },
+                "page3": {
+                    "elements": {
+                        "question2": {
+                            "title": "這是問卷問題 2。"
+                        }
+                    }
+                },
+                "page4": {
+                    "elements": {
+                        "conditionA": {
+                            "title": "是否符合A條件？",
+                            "choices": [
+                                { "value": "yes", "text": "是" },
+                                { "value": "no", "text": "否" }
+                            ]
+                        }
+                    }
+                },
+                "result_a": {
+                    "description": "結果頁面 A：恭喜！您符合條件。"
+                },
+                "result_b": {
+                    "description": "結果頁面 B：抱歉，您不符合條件。"
+                },
+                "completeText": "完成"
             }
         },
         "pages": [
@@ -73,19 +117,44 @@ $(function() {
                 "elements": [
                     {
                         "type": "text",
-                        "name": "name",
+                        "name": "question1",
+                        "isRequired": true,
                         "title": {
-                            "default": "What is your name?",
-                            "zh-tw": "您的姓名是？"
+                            "default": "This is Survey Question 1.",
+                            "zh-tw": "這是問卷問題 1。"
                         }
-                    },
+                    }
+                ]
+            },
+            {
+                "name": "page3",
+                "elements": [
                     {
-                        "type": "number",
-                        "name": "age",
+                        "type": "text",
+                        "name": "question2",
+                        "isRequired": true,
                         "title": {
-                            "default": "How old are you?",
-                            "zh-tw": "您的年齡是？"
+                            "default": "This is Survey Question 2.",
+                            "zh-tw": "這是問卷問題 2。"
                         }
+                    }
+                ]
+            },
+            {
+                "name": "page4",
+                "elements": [
+                    {
+                        "type": "radiogroup",
+                        "name": "conditionA",
+                        "isRequired": true,
+                        "title": {
+                            "default": "Is condition A met?",
+                            "zh-tw": "是否符合A條件？"
+                        },
+                        "choices": [
+                            { "value": "yes", "text": { "default": "Yes", "zh-tw": "是" } },
+                            { "value": "no", "text": { "default": "No", "zh-tw": "否" } }
+                        ]
                     }
                 ]
             }
@@ -97,19 +166,19 @@ $(function() {
             "default": "Complete",
             "zh-tw": "完成"
         }
-    };
+    }
 
     var survey = new Survey.Model(surveyJSON);
 
     $("#surveyElement").Survey({
         model: survey,
-        onCurrentPageChanged: function(sender, options) {
+        onCurrentPageChanged: function (sender, options) {
             if (sender.currentPage.name === 'page2') {
                 var selectedLanguage = sender.data.language;
                 sender.localeValue = selectedLanguage;
             }
         },
-        onComplete: function(sender, options) {
+        onComplete: function (sender, options) {
             $("#surveyElement").hide();
             $("#result").text(JSON.stringify(sender.data)); // 這裡您可以根據回答產生結果連結
             $("#result").show();
